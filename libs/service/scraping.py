53beeec3750e1c97d21fff1d7e62a538b40203fc
ryosyntaxx <riodwi12174@gmail.com>
2023-12-10 20:57:44 +0700

    change flow add dict

--- libs/service/scraping.py
+++ libs/service/scraping.py
@@ -1,20 +1,47 @@
+from time import sleep
 import requests
 import json
+from json import dumps
 from pyquery import PyQuery
 from libs.utils.parser import HtmlParser
+from libs.utils.Writers import Writer
 
 class Scrapper:
     def __init__(self) -> None:
-        self.__parser = PyQuery()
-        self.__api = 'https://www.tiktok.com/api/comment/list/?WebIdLastTime=1702206311&aid=1988&app_language=ja-JP&app_name=tiktok_web&aweme_id=7170139292767882522&browser_language=id&browser_name=Mozilla&browser_online=true&browser_platform=Win32&browser_version=5.0 (Windows)&channel=tiktok_web&cookie_enabled=true&count=20&current_region=JP&cursor=0&device_id=7310920380687025682&device_platform=web_pc&enter_from=tiktok_web&focus_state=true&fromWeb=1&from_page=video&history_len=6&is_fullscreen=false&is_non_personalized=false&is_page_visible=true&os=windows&priority_region=&referer=&region=ID&root_referer=https://www.google.com/&screen_height=1080&screen_width=1920&tz_name=Asia/Jakarta&verifyFp=verify_lpzdpjp3_TwahSIGy_2yrZ_4BVq_BHxe_soMok4JmM93R&webcast_language=id-ID&msToken=Tt6BhEJ8oFCJMm0tPlQWz36tG-sCTqg49Yt65laVyS011Q5-DMH28smioMMk8Y-pgsCPbe3of-kByoRzETsxxVXz-IPV7-FEy20z5mbUqPHaeDXQJ46tcccS8g==&X-Bogus=DFSzsIVO7RJANj6qtuhRDU9WcBjv&_signature=_02B4Z6wo00001nUrvAAAAIDCdSu8A6lfv5Z1KbiAAPhOef'
-
+        # self.__parser = PyQuery()
+        self.__api = "https://www.tiktok.com/api/comment/list/?aid=1988&aweme_id=7170139292767882522&channel=tiktok_web&cookie_enabled=true&count=20&cursor=0&enter_from=tiktok_web&focus_state=true&fromWeb=1&msToken=Tt6BhEJ8oFCJMm0tPlQWz36tG-sCTqg49Yt65laVyS011Q5-DMH28smioMMk8Y-pgsCPbe3of-kByoRzETsxxVXz-IPV7-FEy20z5mbUqPHaeDXQJ46tcccS8g==&X-Bogus=DFSzsIVO7RJANj6qtuhRDU9WcBjv&_signature=_02B4Z6wo00001nUrvAAAAIDCdSu8A6lfv5Z1KbiAAPhOef"
     
-    def extract_data(self, url: str):
+    def extract_data(self):
         response = requests.get(self.__api)
+
+        print(response.json()['total'])
+
+        content = {
+                'desc': response.json()['comments'][0]['share_info']['title'].split('#')[0],
+                'url': response.json()['comments'][0]['share_info']['url'],
+                'total_command': response.json()['total'],
+                'hastags': ['#'+tag.replace(' ', '') for tag in response.json()['comments'][0]['share_info']['title'].split('#')[1:]]
+            }
+
+        print(content)
+        # for command in response.json()['comments']:
+        #     print(command['user']['nickname'])
+
+
+        #     result = {
+        #         'uid': command['user']['uid'],
+        #         'unique_id': command['user']['unique_id'],
+        #         'nickname': command['user']['nickname'],
+        #     }
+        #     # print(command)
+        #     sleep(2)
+
+
+        # with open('private/test.json', 'w', encoding="utf-8") as file:
+        #     json.dump(response.json(), file, indent=2, ensure_ascii=False)
         
-        
-        pass
 
 
     def ex(self):
         pass
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from libs import Scrapper
+
+if __name__ == '__main__':
+    scrap = Scrapper()
+    scrap.extract_data()
